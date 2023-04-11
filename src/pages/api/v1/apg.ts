2f635f46d54de66a9a98db9b314094a890040d15
alari777 <alari777@gmail.com>
2023-04-11 23:30:40 +0300

    refactored `callApgTool` function

--- src/pages/api/v1/apg.ts
+++ src/pages/api/v1/apg.ts
@@ -1,17 +1,26 @@
-import { createHandler, Get } from 'next-api-decorators';
+import { createHandler, Get, Query } from 'next-api-decorators';
 import { execSync } from 'child_process';
 
 class FetchApgData {
   // GET /api/v1/apg
   @Get()
-  async callApgTool(): Promise<any> {
-    const output = execSync('apg', {
+  async callApgTool(
+    @Query('params') params: string
+  ): Promise<{ data: string[] }> {
+    // console.log(params);
+    const output = execSync(`apg ${params}`, {
       encoding: 'utf8',
       maxBuffer: 250 * 1024 * 1024,
-    }).toString();
+    })
+      .toString()
+      .trim();
+
     console.log('Output was:\n', output);
+
+    const apgPasswords = output.split('\n');
+
     return {
-      data: output,
+      data: apgPasswords,
     };
   }
 }
